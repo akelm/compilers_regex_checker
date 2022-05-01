8ab09edaa64706c7fba3a7a8f19696d653543348
ania <annamariakelm@gmail.com>
2022-05-01 03:52:44 +0200

    Zmiana w schemacie automatu

--- automaton.py
+++ automaton.py
@@ -6,24 +6,24 @@ from state import State
 
 class Automaton:
     def __init__(self, first: str, states_list, alphabet: Set[str]):
-        self.states_list = states_list
-        self.on_input = defaultdict(lambda: State.on_other)
+        self._states_list = states_list
+        self._on_input = defaultdict(lambda: State.on_other)
         if len(alphabet) < 2:
             raise "too short alphabet"
         if first in alphabet:
             opposite = alphabet.difference({first}).pop()
-            self.on_input[first] = State.on_first
-            self.on_input[opposite] = State.on_opposite
-        self.state_number = 0
+            self._on_input[first] = State.on_first
+            self._on_input[opposite] = State.on_opposite
+        self._state_number = 0
 
     def new_state(self, x: str) -> bool:
-        current_state: State = self.states_list[self.state_number]
-        next_state_num = self.on_input[x](current_state)
+        current_state: State = self._states_list[self._state_number]
+        next_state_num = self._on_input[x](current_state)
         if next_state_num is None:
             return False
         else:
-            self.state_number = next_state_num
+            self._state_number = next_state_num
             return True
 
     def check_accepting(self):
-        return self.states_list[self.state_number].accepting
+        return self._states_list[self._state_number].accepting
